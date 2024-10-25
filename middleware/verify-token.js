12f795873253f1789b74f9762f639391f1db2923
joemarney <marney.joe@gmail.com>
2024-10-25 14:32:41 +0100

    verify token created

---
+++ middleware/verify-token.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+// ! Model
+const User = require("../models/user");
+
+export async function verifyToken(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) throw new Error("Token not present in authorisation header");
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(payload._id);
+
+    if (!user) throw new Error("User does not exist");
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+}
