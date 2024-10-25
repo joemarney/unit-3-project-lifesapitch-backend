737d6d920b92baa60d2a002294addd105d5d4f40
the-mostest <fbmcdougall@gmail.com>
2024-10-25 14:39:52 +0100

    Adjsuted signup Route

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,32 +21,49 @@ const User = require('../models/user')
 
 
 router.post('/signUp', async (req, res) => {
-    const { username, password, confirmPassword, email } = req.body
 
-    if (password !== confirmPassword) {
-        console.log('Passwords dont match')
-        return res.status(406).json({ message: 'Unauthorized' })
+    try {
+        const { username, password, confirmPassword, email } = req.body
+
+        if (password !== confirmPassword) {
+            console.log('Passwords dont match')
+            return res.status(406).json({ message: 'Unauthorized' })
+        }
+        const userInDb = User.findOne(username)
+
+        if (userInDb) {
+            console.log('This user already exists')
+            return res.status(406).json({ message: 'Unauthorized' })
+        }
+
+        req.body.hashedPassword = bcrypt.hashSync(hashedPassword, 12)
+
+        const user = User.create(req.body)
+
+        const payload = {
+            username: user.username,
+            _id: user._id
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '24h'
+        })
+
+        return res.status(200).json({user: payload, token})
+
+    } catch (error) {
+        console.log(error)
+        console.log('Sign up isnt working')
     }
-    const userInDb = User.findOne(username)
 
-    if (userInDb) {
-        console.log('This user already exists')
-        return res.status(406).json({ message: 'Unauthorized' })
-    }
+})
 
-    req.body.hashedPassword = bcrypt.hashSync(hashedPassword, 12)
 
-    const user = User.create(req.body)
 
-    const payload = {
-        username: user.username,
-        _id: user._id
-    }
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: '24h'})
 
-    })
+
+//! ==================== Sign In =========================
 
 
 
@@ -56,18 +73,8 @@ router.post('/signUp', async (req, res) => {
 
 
 
-    //! ==================== Sign In =========================
-
-
-
-
-
-
-
-
-
-    //! ==================== Sign Out =========================
-    // local.storage.remove(token)
+//! ==================== Sign Out =========================
+// local.storage.remove(token)
 
 
 
@@ -78,4 +85,4 @@ router.post('/signUp', async (req, res) => {
 
 
 
-    module.exports = router
+module.exports = router
--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -4,21 +4,22 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 export async function verifyToken(req, res, next) {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
 
-    if (!token) throw new Error("Token not present in authorisation header");
+        if (!token) throw new Error("Token not present in authorisation header");
 
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(payload._id);
+        const user = await User.findById(payload._id);
 
-    if (!user) throw new Error("User does not exist");
+        if (!user) throw new Error("User does not exist");
 
-    req.user = user;
+        req.user = user;
 
-    next();
-  } catch (error) {
-    console.log(error);
-  }
+        next();
+    } catch (error) {
+        console.log(error);
+        console.log('Verify token isnt work')
+    }
 }
