e5f598bfb6314f844dd68515b0334b0d81e4f567
the-mostest <fbmcdougall@gmail.com>
2024-10-25 12:04:33 +0100

    Setup Server and Added user Model

---
+++ server.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+
+
+
+app.use(express.json());
+app.use(morgan('dev'))
+app.use(cors({ origin: 'http://localhost:5173' }))
+
+
+
+
+app.listen(3000, () => {
+    console.log('The express app is ready!');
+});
