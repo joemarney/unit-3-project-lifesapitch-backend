3c6f956289132c0500ad952c8363c6354f2fd43c
The-Mostest <fbmcdougall@gmail.com>
2024-11-01 16:04:20 +0000

    CORS issue

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.json());
 app.use(morgan('dev'))
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ origin: process.env.FRONT_END_URL }))
 
 
 
