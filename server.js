8268105870c35625db79a426c204993231f0158d
joemarney <marney.joe@gmail.com>
2024-11-03 11:04:12 +0000

    copied read me from front end to here

--- server.js
+++ server.js
@@ -1,59 +1,37 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan')
-const cors = require('cors')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
-const port = 3000
-
+const port = 3000;
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
-
 
 //! ==================== Middleware ========================= ! //
 
 app.use(express.json());
-app.use(morgan('dev'))
-app.use(cors({ origin: 'http://localhost:5173' }))
-
-
+app.use(morgan("dev"));
+app.use(cors({ origin: process.env.FRONT_END_URL }));
 
 //! ==================== Controllers ========================= ! //
 
-const authRouters = require('./controllers/auth')
-const campsitesRouters = require('./controllers/campsites')
-
-
-
-
+const authRouters = require("./controllers/auth");
+const campsitesRouters = require("./controllers/campsites");
 
 //! ==================== Routers ========================= ! //
 
-app.use('/auth', authRouters)
-app.use('/campsites', campsitesRouters)
-
-
-
+app.use("/auth", authRouters);
+app.use("/campsites", campsitesRouters);
 
 //! ==================== Homepage '/' ========================= ! //
 
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
-    console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
